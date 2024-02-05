8fe128272af0b82ea48121770211de90b3d32cf9
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-02-06 00:56:16 +0900

    terraform output

--- main.tf
+++ main.tf
@@ -31,3 +31,8 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+output "public_ip" {
+  value   = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
