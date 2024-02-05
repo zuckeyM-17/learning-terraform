c27424701fb5284cc589d47f0ebf48a78f2826bc
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-02-06 00:52:21 +0900

    http server

--- main.tf
+++ main.tf
@@ -1,8 +1,33 @@
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-07c589821f2b353aa"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
   tags = {
     Name = "example-instance"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
