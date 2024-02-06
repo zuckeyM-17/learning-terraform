1caeea1de38282e4f30c8312f7fe68cd0cf76f24
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-02-07 03:34:10 +0900

    create ASG

--- main.tf
+++ main.tf
@@ -5,20 +5,43 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-07c589821f2b353aa"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id           = "ami-07c589821f2b353aa"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  user_data_replace_on_change = true
-  tags = {
-    Name = "example-instance"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -30,9 +53,4 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-output "public_ip" {
-  value   = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
 }
