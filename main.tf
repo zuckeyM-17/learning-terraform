4a846b79a523ba7efdb6163f914174266143c752
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-02-13 23:50:58 +0900

    remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-zuckey-learning"
+    key    = "global/s3/terraform.tfstate"
+    region = "ap-northeast-1"
+    dynamodb_table = "terraform-up-and-running-lock"
+    encrypt = true
+  }
+}
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
@@ -131,14 +140,51 @@ resource "aws_lb_target_group" "asg" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state"
+  bucket = "terraform-up-and-running-state-zuckey-learning"
 
   lifecycle {
     prevent_destroy = true
   }
 }
 
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "terraform-up-and-running-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 output "alb_dns_name" {
   value     = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
+}
+
+output "s3_bucket_arn" {
+  value     = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value     = aws_dynamodb_table.terraform_lock.name
+  description = "The name of the DynamoDB table"
 }
